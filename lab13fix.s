7aae57ae212f4e2310e67c9687826cc577da8334
FREDRIK OLSSON <froa16@g414-0005>
2017-04-18 15:27:02 +0000

    Funktionell Labb 1

--- lab13fix.s
+++ lab13fix.s
@@ -1,10 +1,7 @@
 .data
 
-factorialNumber: .word 10
-
 textA: .asciz "Lab1 , Assignment  3\n"
 textB: .asciz "Factorial of %d is %d\n"
-textC: .asciz "Done\n"
 .text
 .global main
 .extern printf
@@ -14,25 +11,48 @@ Function :  finding maximum value in a zero terminated integer array
 *******************************************************************/
  factorial:
  PUSH {lr}
- CMP r2, #10
+ MOV r6, r0
+ CMP r6, #0
  BEQ base
- CMP r2, #0
+
+ CMP r6, #11
  BEQ finnish
 
- MUL r1, r2
- SUB r2, #1
- BL factorial
+ MOV r0, r6
+ MOV r1, r5
+ BL print
+ ADD r6, #1
+ MUL r5, r6
+ MOV r0, r6
+
+ BAL recursive
 
  base:
- MOV      r1, #1
- LDR r2, [r0]
- MUL r1, r2
- SUB r2, #1
+ MOV r5, #1
+ MOV r0, r6
+ MOV r1, r5
+ BL print
+ ADD r6, #1
+ MOV r0, r5
+
+ recursive:
  BL factorial
 
-
  finnish:
- MOV r0, r1
+ MOV r0, r5
+ POP {pc}
+
+
+
+
+
+
+ print:
+ PUSH {lr}
+ MOV r2, r1
+ MOV r1, r0
+ LDR r0, =textB
+ BL printf
  POP {pc}
 
 
@@ -43,14 +63,7 @@ main:
 PUSH {lr}
 LDR      r0 , =textA
 BL        printf
-LDR      r0, =factorialNumber
-LDR      r2, [r0]
-MOV      r1, #1
+MOV r0, #0
 BL        factorial
-MOV      r1 , r0
-LDR      r0 , =textB
-BL        printf
-LDR      r0 , =textC
-BL        printf
 POP {pc}
 .end
