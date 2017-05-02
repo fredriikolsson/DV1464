2f87215b390018034c551e05a3f6781339f85cec
FREDRIK OLSSON <froa16@g414-0005>
2017-05-02 15:15:34 +0000

    Räknat GPIO_ADDR och den andra beräkningen

--- gpioaddr.c
+++ gpioaddr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int  gpio_addr = (((0x3F200000) & 0x00FFFFFF) + (((0x3F200000) >> 4) & 0x0F000000) + 0xF0000000);
+  int gpio_addr = (((0x3F200000) & 0x00FFFFFF) + (((0x3F200000) >> 4) & 0x0F000000) + 0xF0000000);
 	printf("%x\n", gpio_addr);
 
   int test1 = (gpio_addr+ (4*(22/10)));
@@ -18,8 +18,10 @@ int main() {
  * (gpio_addr+ (4*(GPIO_NR/10))) |= (1<<(((GPIO_NR)%10)*3));
  * (gpio_addr+ (4*(7 + (GPIO_NR/32)))) = 1 << (GPIO_NR% 32);
  *
+ * GPIO_NR: OUTPUT 17,18,27,22 INPUT: 10, 9
+ *
  * Rätt svar:
- * gpio_addr. 0xf3200000
+ * gpio_addr = 0xf3200000
  * 1. 0xf3200008
  * 2. 40
  * 3. 0xf320001c
